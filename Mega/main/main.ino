f43adcdf04da9b614485bb41c252877b7915af41
kiran4399 <klekkala@usc.edu>
2018-12-03 11:55:44 -0800

    base code

--- Mega/main/main.ino
+++ Mega/main/main.ino
@@ -1,8 +1,12 @@
 
-
 //Sender Code
 //#include "RCrecieve.h"
+#include "Constants.h"
 #include "DifferentialDrive.h"
+#include "Encoder.h"
+#include "Motor.h"
+#include "PositionControl.h"
+#include "SpeedControl.h"
 
 
 
@@ -16,24 +20,20 @@
 
 
 
+LeftMotor = Motor(dir1, lbrake1, hbrake1, pwm1);
+RightMotor = Motor(dir2, lbrake2, hbrake2, pwm2);
+
+LeftEncoder = Encoder(ch1a, ch1b, ch1c);
+RightEncoder = Encoder(ch2a, ch2b, ch3c);
+
+LeftSpeedControl = SpeedControl(LeftMotor, LeftEncoder);
+RightSpeedControl = SpeedControl(RightMotor, RightEncoder);
+
+LeftPositionControl = PositionControl(LeftSpeedControl);
+RightPositionControl = PositionControl(RightSpeedControl);
 
 
-LeftMotor = Motor();
-RightMotor = Motor();
-
-LeftEncoder = Encoder(2, 3, 18);
-RightEncoder = Encoder(19, 20, 21);
-
-
-
-LeftPositionControl = PositionControl();
-LeftPositionControl = PositionControl();
-
-LeftSpeedControl = SpeedControl();
-LeftSpeedControl = SpeedControl();
-
-
-
+Driver = DifferentialDrive(LeftPositionControl, RightPositionControl, 10, 20);
 
 
 
@@ -43,28 +43,39 @@ LeftSpeedControl = SpeedControl();
 
 void setup() {
   //rcsetup();
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  //TCCR2A = _BV(COM2A0) | _BV(COM2B1) | _BV(WGM20);
-  //TCCR2B = _BV(WGM22) | _BV(CS22);
-  //OCR2A = 250;
-  //OCR2B = 250;
 
+
+  attachInterrupt(0, magnet_detect1, RISING);//Initialize the interrupt pin
+  pinMode(1, INPUT);
   pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(7, INPUT);
   
+  pinMode(40, INPUT);
+  pinMode(41, INPUT);
+  pinMode(42, INPUT);
+  pinMode(43, INPUT);
+  pinMode(44, INPUT);
+  pinMode(45, INPUT);
   
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+  pinMode(6, OUTPUT);
+  digitalWrite(6, LOW);
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
   pinMode(8, OUTPUT);
-  digitalWrite(8, HIGH);
+  digitalWrite(8, LOW);
   pinMode(9, OUTPUT);
   digitalWrite(9, HIGH);
   pinMode(10, OUTPUT);
   digitalWrite(10, HIGH);
   pinMode(11, OUTPUT);
-  digitalWrite(11, HIGH);
-  pinMode(12, OUTPUT);
+  digitalWrite(11, LOW);
 
   //Serial writing
   Serial.begin(115200);
@@ -78,25 +89,25 @@ void loop() {
   float b = pulseIn(3, HIGH, 250000); // Read the pulse width of
   float c = pulseIn(4, HIGH, 250000); // Read the pulse width of
   float d = pulseIn(7, HIGH, 250000); // Read the pulse width of
-  float ver = ((c-1000)/1000)*255;
-  float hor = ((b-1000)/1000)*255;
-  float scaler = a;
-  float reverse = d;
-
+  
   //c is right vertical
   //a is left vertical
   //b is right horizontal
   //d is reverse or forward switch
 
+  float ver = ((c-1000)/1000)*255;
+  float hor = ((b-1000)/1000)*255;
+  float scaler = a;
+  float reverse = d;
 
-if (ver <=1500 && digitalRead(reverse) == HIGH){
+
+  if (ver <=1500 && digitalRead(reverse) == HIGH){
      digitalWrite(relay, LOW);
-  }
+    }
 
   else{
     CalculateTankDrive(hor, ver)
   }
-  //digitalWrite(10,LOW);
-  //digitalWrite(11,HIGH);
+  
   //delay(100);
 }
