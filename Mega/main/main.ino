ef8a4b324b014b8f1a19265877e29d2bc024f9f3
kiran4399 <kiran4399@gmail.com>
2019-04-17 16:40:34 -0700

    update

--- Mega/main/main.ino
+++ Mega/main/main.ino
@@ -94,7 +94,7 @@ void setup() {
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 0;
-OCR1A = 5000;            // compare match register 16MHz/256/2Hz
+OCR1A = 1000;            // compare match register 16MHz/256/2Hz
   TCCR1B |= (1 << WGM12);   // CTC mode
   TCCR1B |= (1 << CS12);    // 256 prescaler 
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
@@ -109,9 +109,10 @@ OCR1A = 5000;            // compare match register 16MHz/256/2Hz
 ISR(TIMER1_COMPA_vect){
   //LeftEncoder.getSpeed(LeftMotor._forward);
   //Serial.println("timer");
-  //if (once == 0)
+  if (once == 0){
     //LeftSpeedControl.adjustPWM();
     //RightSpeedControl.adjustPWM();
+  }
 
 };
 
@@ -132,8 +133,8 @@ if (once == 1){
   
   
   LeftMotor.setFwd();
-  RightMotor.setFwd();
-  //LeftSpeedControl.setSpeed(720);
+  //RightMotor.setFwd();
+  //LeftSpeedControl.setSpeed(360);
   //RightSpeedControl.setSpeed(720);
   once = 0;
 }
